958ef04f5da82b1d13366fc78c0ef1f0c601a1c7
Ravi Baka <Ravi.Baka@klp.no>
2020-04-06 14:18:19 +0200

    Updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
 insert into customer
-values(10001,'Manju', 'manju@gmail.com', 'ACTIVE');
+values(10001,'Name1', 'Name1@gmail.com', 'ACTIVE');
 
 insert into customer
-values(10002,'Ravi', 'ravi@gmail.com', 'REJECTED');
+values(10002,'Name2', 'Name2@gmail.com', 'REJECTED');
 
 insert into customer
-values(10003,'Suchi', 'suchi@gmail.com', 'PENDING');
+values(10003,'Name3', 'Name3@gmail.com', 'PENDING');
 
 insert into customer
-values(10004,'Test', 'test@gmail.com', 'REGISTERED');
+values(10004,'Name4', 'Name4@gmail.com', 'REGISTERED');
